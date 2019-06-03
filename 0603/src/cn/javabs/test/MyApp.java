4bea1e6b959c7a3c8ffc9e88acc570475a2519c2
mryang1688 <mryang@mingyang888.cn>
2019-06-03 11:08:51 +0800

    旺财第2次提交的代码

--- 0603/src/cn/javabs/test/MyApp.java
+++ 0603/src/cn/javabs/test/MyApp.java
@@ -3,5 +3,6 @@ package cn.javabs.test;
 public class  MyApp {
     public static void main(String[] args) {
         System.out.println("Hello VersionControl");
+        System.out.println("你愁啥");
     }
 }
